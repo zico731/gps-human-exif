5468b5b50de8353382f0263e7b99314aa4c40295
zico731 <zico731@hotmail.com>
2022-11-27 21:13:45 +0100

    on affiche le nom de la photo à traiter

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Comment voir exactement où une photo a été prise grâce aux metadonnées EXIF
 Traduit les coordonnées gps d'une photo géolocalisée en localisation lisible (numéro de rue, rue, ville, département, pays)
 
 ## usage ##  
- gps-human-exif-sh  \<photo_path\>
-
+gps-human-exif-sh  \<photo_path\>  
+  
+### traitement en masse sur des images 
+find /mnt/c/Users/zico731/Downloads/ -name "*.jpg" -print0 | xargs -0 -n1 -l /tmp/gps-human-exif/gps-human-exif.sh
--- gps-human-exif.sh
+++ gps-human-exif.sh
@@ -2,8 +2,13 @@
 # Créateur : zico731
 # Date 27/11/2022
 # Comment voir exactement où une photo a été prise grâce aux metadonnées EXIF
+# usage : gps-human-exif.sh
+#         find /mnt/c/Users/paco/Downloads/ -name "*.jpg" -print0| xargs -0 -n1 -l /tmp/gps-human-exif/gps-human-exif.sh  # multiple
 
-photo=$1
-[[ -z $1 ]] && (echo  -e "\n usage : ${0##*/} <photo_path>\n";exit 1)
-coord=$(exiftool -c "%+ .6f" ${photo} |awk -F: '/GPS Position/ {print $2}'| tr -d ' +'| tr ',' ' ') && read lat lon <<< "$coord"
+photo="${1}"
+[[ -z "$1" ]] && (echo  -e "\n usage : ${0##*/} <photo_path>\n";exit 1)
+echo "${photo}"
+coord=$(exiftool -c "%+ .6f" "${photo}" |awk -F: '/GPS Position/ {print $2}'| tr -d ' +'| tr ',' ' ') && read lat lon <<< "$coord"
 curl -s "https://nominatim.openstreetmap.org/reverse?format=xml&lat=${lat}&lon=${lon}"|grep -oP '(?<=result).*(?=</result>)'|cut -d'>' -f2|sed 's/, /\n/g' 
+echo ""
+
