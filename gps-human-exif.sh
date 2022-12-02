039d33ad0d1f61593c0b354f7e98f84d72d0eb22
zico731 <zico731@hotmail.com>
2022-12-02 15:18:44 +0100

    gestion des erreurs + visualisation sur une carte geographique

--- gps-human-exif.sh
+++ gps-human-exif.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
-# Créateur : zico731
+# Createur : zico731
 # Date 27/11/2022
-# Comment voir exactement où une photo a été prise grâce aux metadonnées EXIF
+# Comment voir exactement ou une photo a ete prise grace aux metadonnees EXIF
 # usage : gps-human-exif.sh
-#         find /mnt/c/Users/paco/Downloads/ -name "*.jpg" -print0| xargs -0 -n1 -l /tmp/gps-human-exif/gps-human-exif.sh  # multiple
+# find /mnt/c/Users/paco/Downloads/ -name "*.jpg" -print0| xargs -0 -n1 -l /tmp/gps-human-exif/gps-human-exif.sh  # multiple
 
 photo="${1}"
-[[ -z "$1" ]] && (echo  -e "\n usage : ${0##*/} <photo_path>\n";exit 1)
-echo "${photo}"
-coord=$(exiftool -c "%+ .6f" "${photo}" |awk -F: '/GPS Position/ {print $2}'| tr -d ' +'| tr ',' ' ') && read lat lon <<< "$coord"
-curl -s "https://nominatim.openstreetmap.org/reverse?format=xml&lat=${lat}&lon=${lon}"|grep -oP '(?<=result).*(?=</result>)'|cut -d'>' -f2|sed 's/, /\n/g' 
-echo ""
 
+if [ -z "$photo" ]
+then
+        echo  -e "\n usage : ${0##*/} <photo_path>\n"
+elif [ ! -f "$photo" ] 
+then    
+        echo "$photo n'existe pas"
+
+else    
+        coord=$(exiftool -c "%+ .6f" ${photo} |awk -F: '/GPS Position/ {print $2}'| tr -d ' ')
+        if [ -z "$coord" ]
+        then 
+                echo "$photo ne contient pas de coordonnees gps"
+        else
+                lat=$(cut -d',' -f1 <<< ${coord})
+                lon=$(cut -d',' -f2 <<< ${coord})
+                echo -e "${photo} a ete geocalise avec les coordonnees ${coord} correspondant : \n"
+
+                curl -s "https://nominatim.openstreetmap.org/reverse?format=xml&lat=${lat#*\+}&lon=${lon#*\+}"|grep -oP '(?<=result).*(?=</result>)'|cut -d'>' -f2|sed 's/, /\n/g' 
+
+                echo ""
+                read -p "Souhaitez-vous visualiser sur une carte (o/N)? " reponse
+                [ "$reponse" == "o" ] && sensible-browser "http://geo.klein-computing.de/gpx_tool.html#${lat},${lon}" 2>/dev/null
+	fi      
+fi
